a2d231f84524664ce142cf37b1450dd6b948f570
krzysztof baranski <krisbaranski@gmail.com>
2023-05-15 09:35:00 +0200

    feat: write head: title, description, favicon

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <title>BINTA - Buddhist Institute of Tibetan Art</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        name='description'
+        content='Website providing basic informations about buddhist art'
+      />
+      <link rel='icon' href='../public/favicon.ico' />
+    </>
+  );
+}
