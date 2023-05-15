2d76243a2dc8bc38cde353c5ad9f8b35a7a9d668
krzysztof baranski <krisbaranski@gmail.com>
2023-05-15 09:56:52 +0200

    chore: prepare for deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  basePath: '/gh-pages-test',
+  output: "standalone",
+  basePath: "/gh-pages-test",
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
