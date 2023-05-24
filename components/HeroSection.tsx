103fa6026a285c47b4ce00647882a8cf8ee6654f
krzysztof baranski <krisbaranski@gmail.com>
2023-05-24 10:54:00 +0200

    docs: add new text

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -17,46 +17,61 @@ const HeroSection = () => {
         />
 
         <div className='flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left'>
-          <div className='md:w-1/2 '>
-            <h1 className='text-center text-4xl md:text-6xl font-bold my-6 md:text-right'>
+          <div className='md:w-1/2'>
+            <h1 className='text-center text-4xl md:text-6xl font-bold  md:text-right'>
               Buddhist Institute of Tibetan Art
             </h1>
           </div>
 
           <div className='text-center md:w-1/2 md:text-left'>
             <p>
+              BINTA is collecting informations and providing knowlegde of
+              tibetan buddhist art.
+              <br />
+              <br />
+              Institute is located in Braunschweig, Germany.
+              <br />
+              Since 2006 knowledge of clay statue building and thangka painting
+              has been passed on to western students by recognized masters.
+              <br />
+              In October 2015, Lama Ole Nydahl gave permission to build staues
+              for the Diamond Way Buddhist centers and asked his students to
+              organize a school to learn skills of building and painting in
+              traditional buddhist way.
+            </p>
+          </div>
+        </div>
+
+        <hr className='w-full h-0.5 mx-auto my-20 bg-neutral-600 border-0'></hr>
+
+        <div className='flex flex-col space-y-10 justify-center align-center md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left'>
+          <div className='text-center md:w-1/2 md:text-left'>
+            <p>
               Statues and thangkas (scroll paintings) are inseparable from
-              Tibetan Buddhism. The manifold Buddha forms are useful to bring to
+              Tibetan Buddhism. The varied Buddha forms are useful to bring to
               mind the Buddha aspects during meditation. These cause responses
               in our mind. In order to make statues and thangkas work, they have
               to be produced in accordance to defined standards and they need to
               possess attributes that are described in the meditation
               instructions. For their transmission experienced masters are
-              needed. BINTA is the short form of Buddhist Institute of Tibetan
-              Art, based in the Diamondway Buddhist Center, Braunschweig,
-              Germany.
+              needed.
             </p>
-            <br />
-            <p>
-              Since 2006 the knowledge of building clay statues is being
-              transmitted by acknowledged statue builders to western students in
-              Braunschweig. In October 2015 Lama Ole Nydahl gave us the
-              permission to build statues for the centers and requested the
-              founding of a school in which the art of building statues could be
-              acquired.
-            </p>
-            <br />
+          </div>
+          <div className='text-center md:w-1/2 md:text-left'>
             <p>
               <span className='italic'>
                 “This is our technique, clay is the material in the Himalaya.
-                You ought to build statues for the centers.”{" "}
+                You should build statues for the centers.”
               </span>
+              <br />
               <br />
               Lama Ole Nydahl on the occasion of visiting the BINTA in October
               2015
             </p>
           </div>
         </div>
+
+        <div className='flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left'></div>
       </div>
     </section>
   );
