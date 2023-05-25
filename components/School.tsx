8c7b593b0218c6d2f1e0cd69ec67262758879772
krzysztof baranski <krisbaranski@gmail.com>
2023-05-25 21:44:08 +0200

    docs: add new text

--- components/School.tsx
+++ components/School.tsx
@@ -13,15 +13,15 @@ const AboutSection = () => {
         <div className='flex flex-col space-y-10 justify-center align-center md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left'>
           <div className='text-center md:w-1/2 md:text-left'>
             <p>
-              The goal is to be trained to become independent statue builders
-              and thangka painters. It has become possible to go through a solid
-              formation conducted by statue building masters in building statues
-              in the western hemisphere.
+              We continuously offer courses in which one can get a sense of the
+              tradition of building clay statues that goes back now for more
+              than a thousand years. Meanwhile we have elaborated a curriculum
+              together with statue building masters.
               <br />
               <br />
               In Ladakh, this education lasts between 6 to 17 years while
-              working intensively with the master on a daily basis. During the
-              education, the students draw and model over several years
+              working intensively with the master daily. During the education,
+              the students draw and model over several years.
               <br />
               <br />
               (the duration is due to the student´s capacities and personal
@@ -52,17 +52,19 @@ const AboutSection = () => {
           </div>
           <div className='text-center md:w-1/2 md:text-left'>
             <p>
-              The following Buddha forms and Buddha aspects have already been
-              built under our teacher´s guidance:
+              The following Buddha forms have already been built under our
+              teacher´s guidance:
               <br />
               <br />
               <ol className='list-disc pl-8'>
-                <li>Buddha Shakyamuni,</li>
-                <li>Green or White Liberatice (tib.: Dolmar; sansk.: Tara),</li>
-                <li>Black Coat (tib.: Bernagchen; sansk.: Mahakala),</li>
-                <li>Loving Eyes (tib.: Chenrezig; sanskr.: Avalokitesvara)</li>
-                <li>Guru Rinpoche (tib. ; sanskr.:Padmasambhava)</li>
-                <li> Milarepa, Marpa and other forms.</li>
+                <li> Buddha Shakyamuni,</li>
+                <li> Green Liberatrice (tib.: Dolmar; sansk.: Shyamatara)</li>
+                <li> White Liberatrice (tib.: Dolkar; sansk.: Sitatara)</li>
+                <li> Black Coat (tib.: Bernagchen; sansk.: Mahakala)</li>
+                <li> Loving Eyes (tib.: Chenrezig; sanskr.: Avalokitesvara)</li>
+                <li> Guru Rinpoche (tib. ; sanskr.:Padmasambhava)</li>
+                <li> Milarepa, Marpa</li>
+                <li> 16th Karmapa (tib.:Rangdjung Rigpe Dorje)</li>
               </ol>
               <br />
               <br />
