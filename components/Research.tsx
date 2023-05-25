6d93f2e47e522b5b454e0a73f3ed57a46d20ae8b
krzysztof baranski <krisbaranski@gmail.com>
2023-05-25 21:54:39 +0200

    docs: add new text

--- components/Research.tsx
+++ components/Research.tsx
@@ -4,13 +4,37 @@ import React from "react";
 const Research = () => {
   return (
     <section id='research'>
-      <div className='my-12 pb-12 md:pt-16 md:pb-48'>
+      <div className='my-12 pb-12 md:pt-8 md:pb-48'>
         <h1 className='text-center font-bold text-4xl'>
           Research
           <hr className='w-24 h-1 mx-auto my-4 bg-stone-500 border-0 rounded'></hr>
         </h1>
 
-        <div className='flex flex-col space-y-28'></div>
+        <div className='flex flex-col space-y-12'>
+          <p>
+            Statues are an important aspect of Diamond Way Methods. In addition
+            to the well-known metal statues from Nepal, clay statues are also
+            widespread throughout the Himalayan areas of Bhutan, Tibet and
+            Ladakh. The main aspects of Buddhist meditations have been created
+            out of clay directly in the monasteries for more than 1,000 years.
+            <br />
+            Clay statues, as stated by Lama Ole Nydahl in Braunschweig in 2015,
+            “Our methods and material are that of Tibet”. The art of making clay
+            statues has a long tradition. The specialized library for
+            Traditional Buddhist art contains valuable books on Buddhist art,
+            exhibition catalogues, as well as doctoral thesis and publications
+            on this subject.
+            <br />
+            About the clay statue construction in Ladakh and Brunswick there is
+            a dissertation by Petra Förster
+            {/* <a href='https://www.logos-verlag.de/cgi-bin/engbuchmid?isbn=5063&lng=deu&id='>
+              ´Buddhistischer Lehmstatuenbau in Ladakh´
+            </a> */}
+            The production of the clay statues in Braunschweig is documented and
+            archived. In this way the knowledge about clay statue making and its
+            tradition is preserved in a long term.
+          </p>
+        </div>
       </div>
     </section>
   );
